ad39965e116ee7f66a15e1c3f3f23a60e13638d3
johannes.holzmann <johannes.holzmann@fdmgroup.com>
2021-04-15 09:55:33 +0200

    refactoring:
    removed unused import
    switched from .size to .isEmpty for better readability
    updated some comments

--- src/main/java/solution/MergeIntervalsSolution.java
+++ src/main/java/solution/MergeIntervalsSolution.java
@@ -1,6 +1,5 @@
 package solution;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.LinkedList;
@@ -9,21 +8,22 @@ public class MergeIntervalsSolution {
 	public int[][] merge(int[][] intervallArray) {
 		// Sort intervals by their lower limit
 		Arrays.sort(intervallArray, Comparator.comparingInt(interval -> interval[0]));
-		// New ArrayList for the merged intervals
+		// New LinkedList for the merged intervals
 		LinkedList<int[]> mergedIntervals = new LinkedList<>();
-		//iterate over intervals in original array
+		// Iterate over intervals in original array
 		for (int[] currentInterval : intervallArray) {
-			//if no array is in the merged list or the current array does not overlap with the previous one it is added to the merged list
-			if (mergedIntervals.size() == 0
-					|| (currentInterval[0] > mergedIntervals.getLast()[1])) {
+			// If no array is in the merged list or the current array does not overlap with
+			// the previous one it is added to the merged list
+			if (mergedIntervals.isEmpty() || (currentInterval[0] > mergedIntervals.getLast()[1])) {
 				mergedIntervals.add(currentInterval);
-			} 
-			//Case if the two intervals overlap
+			}
+			// Case if the two intervals overlap
 			else {
-				//The upper limits of the two compared intervals
+				// The upper limits of the two compared intervals
 				int currentIntervalUpperLimit = currentInterval[1];
 				int mergedIntervalUpperLimit = mergedIntervals.getLast()[1];
-				//If the intervals overlap and the upper limit of the current interval is higher, the interval in the merged list is extended
+				// If the intervals overlap and the upper limit of the current interval is
+				// higher, the interval in the merged list is extended
 				if (currentIntervalUpperLimit >= mergedIntervalUpperLimit) {
 					mergedIntervals.getLast()[1] = currentIntervalUpperLimit;
 				}
@@ -41,7 +41,8 @@ public class MergeIntervalsSolution {
 		int[] array4 = { 4, 8 };
 		int[][] intervalls = { array1, array2, array3, array4 };
 		MergeIntervalsSolution mergeIntervalls = new MergeIntervalsSolution();
-		// Test run printing result to console
+
+		// Example test run printing result to console
 		System.out.println("Before merge:");
 		System.out.println(Arrays.deepToString(intervalls));
 		System.out.println("After merge:");
